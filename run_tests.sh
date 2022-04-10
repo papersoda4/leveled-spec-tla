9fc13c6e40e58372c8b9ba3d4607fa61dde408fb
lukas.jukna <lukas.juknae@gmail.com>
2022-04-10 13:57:36 +0300

    improve sh script

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-tlacli check \
-    --cfg ./Leveled/MC_Leveled1.cfg \
-    ./Leveled/MC_Leveled1.tla
+root_dir="./Leveled"
+declare -a test_files=(
+    "MC_Leveled1"
+)
+
+function run_test_file() {
+    tlacli check \
+        --cfg "${root_dir}/$1.cfg" \
+        "${root_dir}/$1.tla"
+}
+
+function run_tests() {
+    for index in "${!test_files[@]}"; do
+        run_test_file "${test_files[index]}"
+    done
+}
+
+run_tests
